32de397702adfb47ed0eef18d2d78f4ba9680691
EG5h <eug033@gmail.com>
2016-01-24 02:11:38 +0300

    mysql utf8 compatiblity

--- connecttobase.py
+++ connecttobase.py
@@ -1,26 +1,31 @@
 # -*- coding: utf-8
 import MySQLdb
 
-card = [[u'LDR', u'None', u'000111'],
-['001', 'None', u'004716111'],
-['017', '##', u'0121'],
+card = [[u'LDR', u'', u'000111'],
+['001', '', u'004716111'],
+['017', '##', u'0121 лыололо'],
 ['020', '##', u'$a 978-5-9922-0646-3'],
 ['041', '0#', u'$a rus'],
 ['100', '1#', u'$a Кружевский']]
 
 
 # подключаемся к базе данных (не забываем указать кодировку, а то в базу запишутся иероглифы)
-db = MySQLdb.connect(host="localhost", user="root", passwd="123", db="bookcards", use_unicode=True, charset='utf-8') #charset='utf-8'
+db = MySQLdb.connect(host="localhost", user="marc", passwd="123", db="marc", use_unicode=True, charset='utf8') #charset='utf-8'
 # формируем курсор, с помощью которого можно исполнять SQL-запросы
+db.set_character_set('utf8')
 cursor = db.cursor()
 
+cursor.execute("SET NAMES utf8;")
+cursor.execute("SET CHARACTER SET utf8")
+cursor.execute("SET character_set_connection=utf8")
 n = 0
 for element in card:
 # подставляем эти данные в SQL-запрос
-    sql = """INSERT INTO bookcards(field, marker, info)
+    sql = u"""INSERT INTO cards(field, marker, info)
         VALUES ('%(field)s', '%(marker)s', '%(info)s')
         """%{"field":card[n][0], "marker":card[n][1], "info":card[n][2]}
 # исполняем SQL-запрос
+    print sql
     cursor.execute(sql)
 # применяем изменения к базе данных
     db.commit()
