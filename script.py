fb3a1efb65c223e1fffc9c0834b1bfb46447ddb2
EG5h <eug033@gmail.com>
2016-01-25 01:31:08 +0300

    loops added

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ import string
 
 parser = argparse.ArgumentParser(description='Выделяем isbn')
 parser.add_argument('--in', required=True, dest='InFileName', help='укажите имя входного файла')
+parser.add_argument('--v', required=False, dest='verbose', action="store_true", help='verbose режим')
 args = parser.parse_args()
 #print (args.InFileName)
 
@@ -20,6 +21,8 @@ def unpack_line(line):
 	line = re.sub(u'^\s+', '', line, 0, re.M)
 	line = re.sub(u'\s+$', '', line, 0, re.M)
 	els = string.split(line, ", ")
+        if( re.search(u'\d+',line) == None):
+          els = []   
 	return els
 
 # открываем исходный csv-файл
@@ -30,16 +33,33 @@ lines = f.readlines()
 lst_isbn = []
 
 for line in lines:
-	els = unpack_line(line)
-	lst_isbn.append(els)
+	lst_isbn+= unpack_line(line)
 #print lst_isbn
 
+# подключаемся к базе данных (не забываем указать кодировку, а то в базу запишутся иероглифы)
+db = MySQLdb.connect(host="localhost", user="marc", passwd="123", db="marc", use_unicode=True,
+						 charset='utf8') #charset='utf-8'
+# формируем курсор, с помощью которого можно исполнять SQL-запросы
+db.set_character_set('utf8')
+cursor = db.cursor()
+
+cursor.execute("SET NAMES utf8;")
+cursor.execute("SET CHARACTER SET utf8")
+cursor.execute("SET character_set_connection=utf8")
+
 n = 0
-for i in lst_isbn:
-	url = 'http://old.rsl.ru/view.jsp?f=7&t=3&v0=' + str(
-		lst_isbn[n]) + '&f=1003&t=1&v1=&f=4&t=2&v2=&f=21&t=3&v3=&f=1016&t=3&v4=&f=1016&t=3&v5=&cc=a1&v=marc&s=2&ce=4'
+for isbn in  lst_isbn:
+	url = 'http://old.rsl.ru/view.jsp?f=7&t=3&v0=' + str(isbn) + '&f=1003&t=1&v1=&f=4&t=2&v2=&f=21&t=3&v3=&f=1016&t=3&v4=&f=1016&t=3&v5=&cc=a1&v=marc&s=2&ce=4'
 	#url = 'http://old.rsl.ru/view.jsp?f=7&t=3&v0=978-5-9922-0646-3&f=1003&t=1&v1=&f=4&t=2&v2=&f=21&t=3&v3=&f=1016&t=3&v4=&f=1016&t=3&v5=&cc=a1&v=marc&s=2&ce=4'
-	data = urllib.urlopen(url).read() #.decode('utf-8') #.encode('utf-8')
+        for http_attempts in range(1, 5):
+            try:
+                data = urllib.urlopen(url).read() #.decode('utf-8') #.encode('utf-8')
+                break
+        # except ??
+        else:
+            print "Network global trouble"
+            exit()
+ 
 	table = re.search('<span class="fcard"><b>.+?(<table.*?</table>)', data, re.DOTALL)
 	try:
 		t3 = ''.join(table.groups())
@@ -51,8 +71,12 @@ for i in lst_isbn:
 	t3 = re.sub(u'\n\n+', '\n', t3, 0, re.M)
 	t3 = re.sub(u'(<strong>)|(</strong>)', '', t3, 0, re.M)
 	t3 = re.sub(u'(<br\s{0,3}/>)', '', t3, 0, re.M)
+	t3 = re.sub(u'(<)(?P<digits>\d{0,13})(>)', '&lt;\g<digits>&gt;', t3, 0, re.M)
 	t3 = re.sub(u'LDR', '000', t3, 0, re.M)
 	t3 = re.sub(u'<td colspan="2"></td>', '', t3, 0, re.M)
+        with open('t3.txt','w') as f:
+           f.write(t3)
+           f.close
 	tree = ET.fromstring(t3)
 	#формируем список из списков с одной карточкой
 	card = []
@@ -62,31 +86,22 @@ for i in lst_isbn:
 		#print child2.text
 		if len(texts) > 1:
 			if int(texts[0]) < 10:
-				myrow = [texts[0], 'No', texts[1]]
+				myrow = [texts[0], '', texts[1]]
 			else:
 				myrow = [texts[0], texts[1][:2], texts[1]]
-		else:
-			myrow = ['x', 'x', 'x']
+                else: continue
 		card.append(myrow)
 
 		#print card
 
-	# подключаемся к базе данных (не забываем указать кодировку, а то в базу запишутся иероглифы)
-	db = MySQLdb.connect(host="localhost", user="marc", passwd="123", db="marc", use_unicode=True,
-						 charset='utf8') #charset='utf-8'
-	# формируем курсор, с помощью которого можно исполнять SQL-запросы
-	db.set_character_set('utf8')
-	cursor = db.cursor()
-
-	cursor.execute("SET NAMES utf8;")
-	cursor.execute("SET CHARACTER SET utf8")
-	cursor.execute("SET character_set_connection=utf8")
 	q = 0
+        if args.verbose:
+            print isbn, n
 	for element in card:
 	# подставляем эти данные в SQL-запрос
 		sql = u"""INSERT INTO cards(isbn, field, marker, info)
         VALUES ('%(isbn)s', '%(field)s', '%(marker)s', '%(info)s')
-        """ % {"isbn":lst_isbn[n], "field": card[q][0], "marker": card[q][1], "info": card[q][2]}
+        """ % {"isbn":isbn, "field": card[q][0], "marker": card[q][1], "info": card[q][2]}
 		# исполняем SQL-запрос
 		#print sql
 		cursor.execute(sql)
@@ -94,7 +109,8 @@ for i in lst_isbn:
 		db.commit()
 		q = q + 1
 
-	# закрываем соединение с базой данных
-	db.close()
 	n = n + 1
-	time.sleep(2)
+	time.sleep(2.2)
+
+# закрываем соединение с базой данных
+db.close()
