974725669a9426adb2793ff3299aa1fe10a2b583
Eugene Gavrish <eug033@gmail.com>
2016-01-30 03:04:32 +0300

    +1

--- aleph-script.py
+++ aleph-script.py
@@ -98,7 +98,7 @@ for isbn in  lst_isbn:
             count +=1
             print count
             if re.search('http://aleph\.rsl\.ru/F/.*func=', link) is not False:
-                TOKEN = re.search('http://aleph\.rsl\.ru/F/(.*?)\?func=', link).group(0)
+                TOKEN = re.search('http://aleph\.rsl\.ru/F/(.*?)\?func=', link).groups()
                 date_now = datetime.datetime.now()
         if len(TOKEN) < 10 :
             print "BAD TOKEN! ==>"+TOKEN
